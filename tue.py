71a8ddddd2c0617d7484d3b4e0972d2d4cd1200a
Mukasa Matthew <matthewmukasa0@gmail.com>
2025-10-28 09:14:28 +0300

    first commit

---
+++ tue.py
@@ -0,0 +1,12 @@
+class Gadget:
+    def start(self):
+        print("Gadget Started")
+
+class Phone(Gadget):
+    def start(self):
+        print("Phone Started")
+
+class Laptop(Gadget):
+    def start(self):
+        print("Laptop Started")
+
